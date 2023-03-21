016a3a65b21df4277dcde55fa080d9f4a7503eaf
Roy Harel <roy.harel06@gmail.com>
2023-03-21 14:15:11 +0200

    Fixes

--- AdptArray.c
+++ AdptArray.c
@@ -14,13 +14,12 @@ struct AdptArray_
 
 PAdptArray CreateAdptArray(COPY_FUNC copy, DEL_FUNC delete, PRINT_FUNC print)
 {
-    PAdptArray adpt_arr = malloc(sizeof(struct AdptArray_));
+    PAdptArray adpt_arr = (PAdptArray) malloc(sizeof(struct AdptArray_));
 
     if (adpt_arr != NULL) 
     {
         adpt_arr->size = 0;
         adpt_arr->elements = NULL;
-
         adpt_arr->copy_func = copy;
         adpt_arr->delete_func = delete;
         adpt_arr->print_func = print;
@@ -60,38 +59,39 @@ Result SetAdptArrayAt(PAdptArray adpt_arr, int index, PElement element)
     }
     else // We would need to extend the array.
     {
-        PElement* temp;
         if (adpt_arr->size == 0)
-            temp = malloc((index+1)*sizeof(PElement));
+            adpt_arr->elements = malloc((index+1)*sizeof(PElement));
         else
-            temp = realloc(adpt_arr->elements, (index+1)*sizeof(PElement));
+            adpt_arr->elements = realloc(adpt_arr->elements, (index+1)*sizeof(PElement));
 
-        if (temp == NULL)
+        if (adpt_arr->elements == NULL)
             return FAIL;
-        
-        adpt_arr->elements = temp;
 
         for (int i = adpt_arr->size; i < index+1; i++)
         {
             adpt_arr->elements[i] = NULL;
         }
+
+        adpt_arr->size = index + 1; 
     }
 
     adpt_arr->elements[index] = adpt_arr->copy_func(element);
-    adpt_arr->size = index + 1;
 
     return SUCCESS;
 }
 
 PElement GetAdptArrayAt(PAdptArray adpt_arr, int index)
 {
-    if (adpt_arr == NULL || adpt_arr->elements == NULL)
+    if (adpt_arr == NULL)
         return NULL;
 
     if (index < 0 || adpt_arr->size-1 < index)
         return NULL;
 
-    return adpt_arr->elements[index];
+    if (adpt_arr->elements[index] == NULL)
+        return NULL;
+
+    return adpt_arr->copy_func(adpt_arr->elements[index]);
 }
 
 int GetAdptArraySize(PAdptArray adpt_arr)
